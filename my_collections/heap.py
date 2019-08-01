e7b3abe834b9996bd8574110abc3b9a1bbbe5e25
dobrosun <mr.dobrogost02@mail.ru>
2019-08-01 16:16:06 +0200

    heap doesn't work

--- my_collections/heap.py
+++ my_collections/heap.py
@@ -1,33 +1,52 @@
 #!/usr/bin/python3
 
 class Heap():
-    def __init__(self):
+    def __init__(self, *args):
         self.arr = []
+        self.size = 0
+        self.heapify(args)
+
+    def heapify(self, args):
+        for iterable in args:
+            for item in iterable:
+                self.add(item)
 
     def add(self, x):
         self.arr.append(x)
+        self.size += 1
         i = len(self.arr) - 1
         while self.arr[(i-1) // 2] < self.arr[i] and i != 0:
             self.arr[i], self.arr[(i-1) // 2] = self.arr[(i-1) // 2], self.arr[i]
             i = (i-1) // 2
-
+        
 
     def pop(self):
         tmp = self.arr[0]
         self.arr[0] = self.arr[-1]
 
+        self.size -= 1
         self.arr.pop()
         i = 0
-        while len(self.arr) >= i*2 + 1 and \
+        
+        while self.size >= i*2 + 1 and \
                   (self.arr[i*2 + 1] > self.arr[i] or (self.arr[i*2 + 2] > self.arr[i])):
+            print(self.arr[i], )
             if self.arr[i*2 + 1] >= self.arr[i*2 + 2]:
                 self.arr[i], self.arr[i*2 + 1] = self.arr[i*2 + 1], self.arr[i]
                 i = i*2 + 1
             else:
                 self.arr[i], self.arr[i*2 + 2] = self.arr[i*2 + 2], self.arr[i]
                 i = i*2 + 2
+
         return tmp
 
+def heapsort(arr):
+    heap = Heap(arr)
+    i = 0
+    while heap.size:
+        arr[i] = heap.pop()
+        i += 1
+    return arr.reverse()
 
 if __name__ == "__main__":
     heap = Heap()
@@ -39,5 +58,6 @@ if __name__ == "__main__":
     heap.add(2)
     heap.add(0)
     print(heap.arr)
-    print(heap.pop())
-    print(heap.arr)
+    for i in range(heap.size):
+        print(heap.pop())
+        print(heap.arr)
--- my_collections/test.py
+++ my_collections/test.py
@@ -2,7 +2,7 @@
 
 import unittest
 from stack import Stack
-
+from heap import Heap, heapsort
 
 class TestStack(unittest.TestCase):
     def test_append(self):
@@ -55,76 +55,31 @@ class TestStack(unittest.TestCase):
         A.clear()
         self.assertEqual(A.arr, [])
 
-"""
-def create_tree():
-    global tree
-    tree = BinaryTree()
-    tree.add(3, '3')
-    tree.add(0, '0')
-    tree.add(7, '7')
-    tree.add(1, '1')
-    tree.add(4, '4')
-    tree.add(8, '8')
-    tree.add(9, '9')
-    tree.add(2, '2')
 
-class TestBinaryTree(unittest.TestCase):
-    
-    def test_add_root(self):
-        create_tree()
-        self.assertEqual(tree.head.key, 3) 
-    
-    def test_adding(self):
-        create_tree()
-        self.assertEqual(tree.head.left.right.key, 1)
-        self.assertEqual(tree.head.left.right.right.key, 2)
-        self.assertEqual(tree.head.right.right.key, 8)
-        self.assertEqual(tree.head.right.left.key, 4)
+class TestHeap(unittest.TestCase):
+    def test_add(self):
+        heap = Heap()
+        heap.add(5)
+        heap.add(3)
+        heap.add(7)
 
-    def test_print(self):
-        create_tree()
-        self.assertEqual(tree.print_in_line(), [3, 0, 7, 1, 4, 8, 2, 9])
-    
-    def test_pop2(self):
-        create_tree()
-        tree.pop(2)
-        self.assertEqual(tree.print_in_line(), [3, 0, 7, 1, 4, 8, 9])
+        heap.add(1)
+        self.assertEqual(heap.arr, [7, 3, 5, 1])
+
+    def test_pop(self):
+        heap = Heap()
+        heap.add(5)
+        heap.add(3)
+        heap.add(7)
+        heap.add(1)
+        self.assertEqual(heap.pop(), 7)
+
+        self.assertEqual(heap.arr, [5, 3, 1])
+
+    def test_heapsort(self):
+        arr = [1, 5, 9, 3, 0, 2]
+        sorted_arr = heapsort(arr)
         
-    def test_pop0(self):
-        create_tree()
-        tree.pop(0)
-        self.assertEqual(tree.print_in_line(), [3, 1, 7, 2, 4, 8, 9])
-    
-    def test_pop3(self):
-        create_tree()
-        tree.pop(3)
-        self.assertEqual(tree.print_in_line(), [2, 0, 7, 1, 4, 8, 9])
-    
-    def test_pop7(self):
-        create_tree()
-        tree.pop(7)
-        self.assertEqual(tree.print_in_line(), [3, 0, 4, 1, 8, 2, 9])
-    
-    def test_pop8(self):
-        create_tree()
-        tree.pop(8)
-        self.assertEqual(tree.print_in_line(), [3, 0, 7, 1, 4, 9, 2])
-                                    
-    def test_pop9(self):
-        create_tree()
-        tree.pop(9)
-        self.assertEqual(tree.print_in_line(), [3, 0, 7, 1, 4, 8, 2])
-
-    def test_pop1(self):
-        create_tree()
-        tree.pop(1)
-        self.assertEqual(tree.print_in_line(), [3, 0, 7, 2, 4, 8, 9])
- 
-    def test_pop4(self):
-        create_tree()
-        tree.pop(4)
-        self.assertEqual(tree.print_in_line(), [3, 0, 7, 1, 8, 2, 9])
-"""
-
+        self.assertEqual(sorted_arr, [0, 1, 2, 3, 5, 9])
 if __name__ == "__main__":
     unittest.main()
